7d5f07b4d03b1a18e81328016d6d7fd102c5095f
onishi <yasuhiro.onishi@gmail.com>
2012-08-17 17:14:52 +0900

    pod修正

--- lib/Text/Hatena.pm
+++ lib/Text/Hatena.pm
@@ -198,7 +198,7 @@ L<onishi E<lt>yasuhiro.onishi@gmail.comE<gt>>
 
 L<Text::Xatena>
 
-L<http://hatenadiary.g.hatena.ne.jp/keyword/%E3%81%AF%E3%81%A6%E3%81%AA%E8%A8%98%E6%B3%95%E4%B8%80%E8%A6%A7>>
+L<http://hatenadiary.g.hatena.ne.jp/keyword/%E3%81%AF%E3%81%A6%E3%81%AA%E8%A8%98%E6%B3%95%E4%B8%80%E8%A6%A7>
 
 =head1 LICENSE
 
